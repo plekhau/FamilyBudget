1eaa049e0dcf76508b10e383737af37fdfd0a6e6
Aliaksandr Plekhau <plekhau@gmail.com>
2026-04-02 23:44:25 -0400

    fix: add Django password validators to RegisterSerializer

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password as django_validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
 from .models import User
 
@@ -9,6 +11,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "email", "display_name", "password", "created_at")
         read_only_fields = ("id", "created_at")
+
+    def validate_password(self, value):
+        try:
+            django_validate_password(value)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(list(e.messages))
+        return value
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
